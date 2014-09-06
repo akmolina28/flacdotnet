62fd5bea8987db16178945ffd4ccb3dbaf4f2b94
akmolina28 <akmolina28@gmail.com>
2014-09-06 15:54:40 -0400

    Added missing files for AudioMetadata

---
+++ FlacDotnetLib/AudioMetadata/FlacMetadata.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlacDotnetLib.AudioMetadata
+{
+    public class FlacMetadata : IAudioMetadata
+    {
+        // default tags
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public string Year { get; set; }
+        public string Comment { get; set; }
+        public int Track { get; set; }
+
+        // flac tags
+        public string Version { get; set; }
+        public string Performer { get; set; }
+        public string Copyright { get; set; }
+        public string License { get; set; }
+        public string Organization { get; set; }
+        public string Description { get; set; }
+        public string Genre { get; set; }
+        public string Date { get; set; }
+        public string Location { get; set; }
+        public string Contact { get; set; }
+        public string ISRC { get; set; }
+        public System.Collections.Specialized.NameValueCollection CustomAttributes { get; set; }
+    }
+}
---
+++ FlacDotnetLib/AudioMetadata/IAudioMetadata.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlacDotnetLib.AudioMetadata
+{
+    public interface IAudioMetadata
+    {
+        string Title { get; set; }
+        string Artist { get; set; }
+        string Album { get; set; }
+        string Year { get; set; }
+        string Comment { get; set; }
+        int Track { get; set; }
+    }
+}
