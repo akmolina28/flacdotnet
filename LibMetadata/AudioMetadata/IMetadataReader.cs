6d85cfb82271118709601298d63c47a2b8077034
akmolina28 <akmolina28@gmail.com>
2014-09-06 20:48:24 -0400

    Changed project name

---
+++ LibMetadata/AudioMetadata/IMetadataReader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlacDotnetLib.AudioMetadata
+{
+    public interface IMetadataReader
+    {
+        string FileName { get; set; }
+        IAudioMetadata ReadMetadata();
+    }
+}
